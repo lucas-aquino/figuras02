b859580ed40b051fe0c0817212e171786ce2b241
Lucas <luckasakino@portonmail.com>
2021-08-16 17:30:05 -0300

    Versión inicial

---
+++ src/figuras02/Rectangulo.java
@@ -0,0 +1,13 @@
+package figuras02;
+
+public class Rectangulo {
+
+	private double ancho, alto;
+
+	public Rectangulo(double ancho, double alto) {
+		this.ancho = ancho;
+		this.alto = alto;
+	}
+	
+
+}
